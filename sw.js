e04cc8b483c093462915e9f527a3184e3efc9683
dpfaker01 <112249907+dpfaker01@users.noreply.github.com>
2026-01-29 14:33:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE_NAME = "kv-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const ASSETS = [
+    "./", 
+    "./index.html", 
+    "./manifest.json", 
+    "./icon_192.png",
+    "./icon_512.png"
+];
 
 self.addEventListener("install", e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
